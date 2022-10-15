eb71448c39f84a6604a4749c7e7cc00dd0a47590
Vaibhaw Kumar <95565240+vaibhawkumar19@users.noreply.github.com>
2022-10-15 22:24:57 +0530

    Add files via upload

---
+++ assignment34_que1.cpp
@@ -0,0 +1,20 @@
+/*1. Write a C++ program to create a file and print “File created successfully” and throw
+an error if file is not created.*/
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	ofstream fout;
+	fout.open("c:\file\abc.txt");
+	
+	if(fout)
+	{
+		cout<<"file created Sucessfully";
+	}
+	else
+	{
+		cout<<"file not Created ";
+	}
+return 0;
+}
